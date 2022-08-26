d287ce3cfc32c97d8319056e22694c32b190125b
Naveed <nak411@gmail.com>
2022-08-25 20:49:02 -0400

    Setup app module and hilt

---
+++ app/src/main/java/com/naveed/mymusicapp/MainApplication.kt
@@ -0,0 +1,33 @@
+package com.naveed.mymusicapp
+
+import android.app.Application
+import android.os.StrictMode
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG) {
+            // Set up Timber
+            Timber.plant(Timber.DebugTree())
+            // Set up strict mode
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
+    }
+}

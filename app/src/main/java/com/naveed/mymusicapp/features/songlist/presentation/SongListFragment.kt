fc284ccc54f364c29c1c737f445ae837a54eb7b9
Naveed <nak411@gmail.com>
2022-08-25 21:58:56 -0400

    Setup permissions

--- app/src/main/java/com/naveed/mymusicapp/features/songlist/presentation/SongListFragment.kt
+++ app/src/main/java/com/naveed/mymusicapp/features/songlist/presentation/SongListFragment.kt
@@ -1,9 +1,15 @@
 package com.naveed.mymusicapp.features.songlist.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.naveed.mymusicapp.databinding.FragmentSongListBinding
@@ -16,14 +22,33 @@ import dagger.hilt.android.AndroidEntryPoint
 class SongListFragment : Fragment() {
 
     private var _binding: FragmentSongListBinding? = null
+
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     private val viewModel: SongListViewModel by viewModels()
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            viewModel.fetchSongs()
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.fetchSongs()
+        val permissionStatus = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            viewModel.fetchSongs()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
     }
 
     override fun onCreateView(

24a3305efb32219e703ca87768a3bd5f2a278fbd
Naveed <nak411@gmail.com>
2022-09-04 21:11:44 -0400

    DelegatePermission based method to permission Handlerr

--- app/src/main/java/com/naveed/mymusicapp/core/presentation/delegates/PermissionHandler.kt
+++ app/src/main/java/com/naveed/mymusicapp/core/presentation/delegates/PermissionHandler.kt
@@ -1,39 +1,43 @@
 package com.naveed.mymusicapp.core.presentation.delegates
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 
+/**
+ * Used for simplifying and extracting out the storage permission flow for [Fragment].
+ */
 interface StoragePermissionHandler {
 
-    fun registerLifecycleOwner(owner: Fragment, onGranted: () -> Unit, onDenied: () -> Unit)
+    /**
+     * Call this method from [Fragment.onCreate] to register for permission flow
+     *
+     * @param owner The fragment making the call
+     * @param onGranted the function invoke when permission is granted
+     * @param onDenied the function to invoke when permission is denied
+     */
+    fun registerStoragePermissionLifecycleOwner(
+        owner: Fragment,
+        onGranted: () -> Unit,
+        onDenied: () -> Unit
+    )
 }
 
 class StoragePermissionHandlerImpl : StoragePermissionHandler, LifecycleEventObserver {
 
+    // Setup lifecycle bound lateinit variable
     private lateinit var fragment: Fragment
     private lateinit var onGranted: () -> Unit
     private lateinit var onDenied: () -> Unit
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
-    private val requestPermissionLauncher = fragment.registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            onGranted()
-        } else {
-            onDenied()
-        }
-    }
-
-    override fun registerLifecycleOwner(
+    override fun registerStoragePermissionLifecycleOwner(
         owner: Fragment,
         onGranted: () -> Unit,
         onDenied: () -> Unit
@@ -42,6 +46,11 @@ class StoragePermissionHandlerImpl : StoragePermissionHandler, LifecycleEventObs
         this.onGranted = onGranted
         this.onDenied = onDenied
         owner.lifecycle.addObserver(this)
+        requestPermissionLauncher = fragment.registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) onGranted() else onDenied()
+        }
     }
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
--- app/src/main/java/com/naveed/mymusicapp/features/songlist/presentation/SongListFragment.kt
+++ app/src/main/java/com/naveed/mymusicapp/features/songlist/presentation/SongListFragment.kt
@@ -1,24 +1,16 @@
 package com.naveed.mymusicapp.features.songlist.presentation
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
-import androidx.core.content.PackageManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.naveed.mymusicapp.core.presentation.delegates.StoragePermissionHandler
 import com.naveed.mymusicapp.core.presentation.delegates.StoragePermissionHandlerImpl
 import com.naveed.mymusicapp.databinding.FragmentSongListBinding
@@ -31,7 +23,7 @@ import timber.log.Timber
  * Contains the UI for displaying the list of songs
  */
 @AndroidEntryPoint
-class SongListFragment : Fragment(){
+class SongListFragment : Fragment(), StoragePermissionHandler by StoragePermissionHandlerImpl() {
 
     private var _binding: FragmentSongListBinding? = null
 
@@ -40,27 +32,14 @@ class SongListFragment : Fragment(){
 
     private val viewModel: SongListViewModel by viewModels()
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            sendEvent(SongListEvent.LoadSongs)
-        } else {
-            Toast.makeText(context, "Permission Denied", Toast.LENGTH_LONG).show()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val permissionStatus = ContextCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.READ_EXTERNAL_STORAGE
+        registerStoragePermissionLifecycleOwner(
+            this,
+            onGranted = { sendEvent(SongListEvent.LoadSongs) },
+            onDenied = { Timber.e("Failed to get permissions")}
         )
-        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-            sendEvent(SongListEvent.LoadSongs)
-        } else {
-            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-        }
         observeState()
     }
 

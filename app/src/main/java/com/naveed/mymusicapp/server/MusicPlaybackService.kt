ab789dcc9f14dd9e9dc11c5bd1632200300c0776
Naveed <nak411@gmail.com>
2022-09-10 20:24:34 -0400

    Added service to manifest

--- app/src/main/java/com/naveed/mymusicapp/server/MusicPlaybackService.kt
+++ app/src/main/java/com/naveed/mymusicapp/server/MusicPlaybackService.kt
@@ -1,8 +1,5 @@
 package com.naveed.mymusicapp.server
 
-import android.media.browse.MediaBrowser
-import android.media.session.MediaSession
-import android.media.session.PlaybackState
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
@@ -10,9 +7,6 @@ import android.support.v4.media.session.PlaybackStateCompat
 import androidx.media.MediaBrowserServiceCompat
 
 class MusicPlaybackService : MediaBrowserServiceCompat() {
-
-    private var mediaSession: MediaSessionCompat? = null
-    private lateinit var stateBuilder: PlaybackStateCompat.Builder
 
     companion object {
         private const val TAG = "MusicService"
@@ -23,20 +17,6 @@ class MusicPlaybackService : MediaBrowserServiceCompat() {
     override fun onCreate() {
         super.onCreate()
 
-
-        // Create a media session
-        mediaSession = MediaSessionCompat(baseContext, TAG).apply {
-            // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
-            stateBuilder = PlaybackStateCompat.Builder()
-                .setActions(PlaybackStateCompat.ACTION_PLAY or PlaybackStateCompat.ACTION_PAUSE)
-            setPlaybackState(stateBuilder.build())
-
-            // MySessionCallback() has methods that handle callbacks from a media controller
-            setCallback(MediaSessionCallback(context = baseContext, mediaSession = this))
-
-            // Set the session's token so that client activities can communicate with it.
-            setSessionToken(sessionToken)
-        }
     }
 
     override fun onGetRoot(
@@ -53,21 +33,6 @@ class MusicPlaybackService : MediaBrowserServiceCompat() {
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
-        // Browsing not allowed
-        if (EMPTY_MEDIA_ROOT_ID == parentId) {
-            result.sendResult(null)
-            return
-        }
 
-        val mediaItems = mutableListOf<MediaBrowserCompat.MediaItem>()
-        // Check if this is the root menu:
-        if (MEDIA_ROOT_ID == parentId) {
-            // Build the MediaItem objects for the top level,
-            // and put them in the mediaItems list...
-        } else {
-            // Examine the passed parentMediaId to see which submenu we're at,
-            // and put the children of that menu in the mediaItems list
-        }
-        result.sendResult(mediaItems)
     }
 }
